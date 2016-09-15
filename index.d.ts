b114b29fc6dcc7238774df9d861d84826d556816
Tingan Ho <tingan87@gmail.com>
2016-09-16 00:27:50 +0800

    Adds typings

---
+++ index.d.ts
@@ -0,0 +1,52 @@
+
+import * as e from '@types/express';
+
+declare module 'express-request-language' {
+    interface Cookie {
+
+        /**
+         * Cookie name.
+         */
+        name: string;
+
+        /**
+         * Cookie option passed in `res.cookie(name, value, [options])`.
+         */
+        options?: e.CookieOptions;
+
+        /**
+         * URL to change preferred language.
+         */
+        url?: string;
+    }
+
+    interface Options {
+
+        /**
+         * Define your supported languages.
+         */
+        languages: string[];
+
+        /**
+         * Query name used for changing language.
+         */
+        queryName?: string;
+
+        /**
+         * Cookie options.
+         */
+        cookie?: Cookie;
+
+        /**
+         * A localization import function. One of the supported languages will be passed as argument.
+         * And the result will be set to `req.localization`. Very handy to be used with L10ns https://l10ns.org.
+         */
+        localizations?: (lang: string) => any;
+    }
+
+    /**
+     * Middleware for language matching.
+     */
+    function requestLanguage(options: Options): any;
+    export = requestLanguage;
+}
